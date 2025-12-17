aa3ce111c42b188e06975916ff448a43d2aa036e
Nurizayudistasari-gif <nurizayudistasaris@gmail.com>
2025-12-17 13:09:58 +0700

    tugas 1

--- tugas piramida.py
+++ tugas piramida.py
@@ -1,9 +1,5 @@
 character = input("masukan karakter: ")
-input_jumlah = int(input("masukan jumlah;"))
 cetak = " " + character + " "
 
-for i in range (1, input_jumlah + 1):
-    print((cetak * i).center(input_jumlah * 2))
-    spaces = " " * (input_jumlah - i)
-    syimbols = character * (2*i-1)
-    print(spaces + cetak * i)
+for i in range (1,20):
+    print((cetak * i).center(80))
